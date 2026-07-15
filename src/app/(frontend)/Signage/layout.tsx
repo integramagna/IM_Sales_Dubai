37e0b91dac4bb7cc981e90c7694d89a215a4a0b7
integramagna <hi@integramagna.com>
2026-07-15 16:11:16 +0530

    first commitdewhh34r3gwrqefeeffvjnjqdwffe

--- src/app/(frontend)/Signage/layout.tsx
+++ src/app/(frontend)/Signage/layout.tsx
@@ -3,9 +3,9 @@ import Header from './Components/Header/Header'
 
 export default function RabtoraLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div style={{ backgroundColor: '#000000' }}>
       <Header />
       {children}
-    </>
+    </div>
   )
 }
